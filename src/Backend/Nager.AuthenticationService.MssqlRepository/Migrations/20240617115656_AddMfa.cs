19fc865cf464ee7741acc3ecd9894bbbc40a58e5
Tino Hager <tino.hager@nager.at>
2024-06-17 13:57:18 +0200

    Add database migration

---
+++ src/Backend/Nager.AuthenticationService.MssqlRepository/Migrations/20240617115656_AddMfa.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nager.AuthenticationService.MssqlRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMfa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLocked",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "MfaActive",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "MfaSecret",
+                table: "Users",
+                type: "varbinary(32)",
+                maxLength: 32,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLocked",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "MfaActive",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "MfaSecret",
+                table: "Users");
+        }
+    }
+}
